ae34bd55fea840695cacc0aca9a39bef22b09561
Z4ppy <Z4ppy@server.fake>
2012-10-27 00:20:00 +0000

    Move the CHANGELOG from the script to a thread (transferred from userscripts.org)

--- script.user.js
+++ script.user.js
@@ -12,49 +12,7 @@
 // @date	    2012-10-27
 // ==/UserScript==
 
-/*
-CHANGELOG:
-0.40   2012-10-27   Added adding torrents to the collage from torrent group and notification pages
-                    Added Chromium support (with Tampermonkey)
-                    Moved icon
-                    Fixed script not running on uploaded/snatched pages 2 and above
-                    Removed window jumping
-                    Removed script update since current versions of both GreaseMonkey and Tampermonkey have built-in auto-update functions
-
-0.36   2012-08-29   Fixed @include URLs and other things related to https://what.cd
-
--- anything up to this point was done by captZEEbo
-
-0.35:	Firefox 4 update
-
-0.34:   Really fixed authkey bug this time :(
-
-0.33:	Fixed authkey bug
-
-0.32:   Fixed bug that broke the script.  Also added authkey reset (useful for password changes)
-
-0.31:	Added auto-sort alphabetically operation.  Changed image for adding something to a collage.
-		Added support for most pages (artist, snatched/uploaded/seeding/leeching lists).  Fixed bug with year sort.  
-
-0.30:	Added a lot of stuff on the manage torrents page.  The much needed "Edit all" button has been added.
-		There is also an "operation" button that allows you to perform operations on a selected range of torrents.
-		Operations include +, -, *, /, reverse all, sort by year, and find missing torrents.
-
-0.21:	Auto-scroll down the torrents.php page, so when you search for something it automatically goes towards the torrents.
-		You can change this in the options menu: right click greasemonkey icon at bottom right, user script commands, "window jump"
-
-0.20:	Added links to collage when necessary.  Added better support on edit, manage, search, browse for collages.  
-		Added error checking in case you change/remove the active collage and then click on add-to-collage image without refreshing torrents page.
-		Fixed bug if you click on album art on a collage then set active collage.  
-		Changed update to check for updates only on collage pages and overall made extension less obtrusive when not in use.
-		Added support for collage.php (I think that's the old version of collages.php).
-		
-0.16:	Fixed a major bug where it did not include torrents.php*
-
-0.15:	Added SSL support.  Got rid of annoying popup in favor of checkmark.
-
-0.1:	Initial release
-*/
+// Changelog can be found at https://userscripts.org/topics/118127
 
 var fullActiveURL = document.URL;
 var site_base_url = fullActiveURL.match(/^(https:\/\/(ssl\.)?what\.cd)/)[1];
